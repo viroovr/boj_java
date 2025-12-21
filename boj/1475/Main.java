8eac744782a48711273f8da862567350903e7ae1
viroovr <tjgusdnjs789@gmail.com>
2025-12-21 17:03:58 +0900

    [Feat] 1475 opt1

--- boj/1475/Main.java
+++ boj/1475/Main.java
@@ -1,5 +1,5 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) throws Exception {
@@ -7,23 +7,22 @@ public class Main {
 
         String N = br.readLine();
 
-        Map<Character, Integer> map = new HashMap<>();
+        int[] cnt = new int[10];
 
         for (char c : N.toCharArray()) {
-            int value = map.getOrDefault(c, 0);
-            map.put(c, value + 1);
+            cnt[c - '0']++;
         }
 
-        int flipSum =  map.getOrDefault('6', 0) + map.getOrDefault('9', 0);
+        int sixNine = cnt[6] + cnt[9];
+        
+        cnt[6] = (sixNine % 2 == 1) ? (sixNine / 2) + 1 : sixNine / 2;
+        cnt[9] = 0;
 
-        map.put('6', flipSum / 2);
-        map.put('9', flipSum / 2);
-        if (flipSum % 2 == 1) {
-            map.put('9', flipSum / 2 + 1);
+        int answer = 0;
+        for (int i = 0; i < 10; i ++) {
+            answer = Math.max(answer, cnt[i]);
         }
 
-        int result = map.values().stream().max(Comparator.naturalOrder()).get();
-
-        System.out.print(result);
+        System.out.print(answer);
     }
 }
