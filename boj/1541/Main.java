e5a0edf61f4766138280dc8b9ea89fd69783e87d
viroovr <tjgusdnjs789@gmail.com>
2025-12-21 13:42:40 +0900

    [Feat] 1541 opt1

--- boj/1541/Main.java
+++ boj/1541/Main.java
@@ -1,58 +1,26 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
         String s = br.readLine();
-        List<Character> op = new ArrayList<>();
-        List<Integer> nums = new ArrayList<>();
 
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
+        String[] minusSplit = s.split("-");
 
-            if (Character.isDigit(ch)) {
-                sb.append(ch);
-            } else {
-                op.add(ch);
-                nums.add(Integer.parseInt(sb.toString()));
-                sb = new StringBuilder();
-            }
+        int ans = sum(minusSplit[0]);
+        for (int i = 1; i < minusSplit.length; i++) {
+            ans -= sum(minusSplit[i]);
         }
-        nums.add(Integer.parseInt(sb.toString()));
 
-        int n = nums.size();
-        int[][] max = new int[n][n];
-        int[][] min = new int[n][n];
-        
-        for (int i = 0; i < min.length; i++) {
-            Arrays.fill(max[i], Integer.MIN_VALUE);
-            Arrays.fill(min[i], Integer.MAX_VALUE);
-            min[i][i] = max[i][i] = nums.get(i);
+        System.out.println(ans);
+    }
+
+    static int sum(String part) {
+        String[] plusSplit = part.split("\\+");
+        int res = 0;
+        for (String p : plusSplit) {
+            res += Integer.parseInt(p);
         }
-        
-        for (int len = 1; len < n; len++) {
-            for (int i = 0; i + len < n; i++) {
-                int j = i + len;
-                for (int k = i; k < j; k++) {
-                    char operation = op.get(k);
-
-                    switch (operation) {
-                        case '+':
-                            min[i][j] = Math.min(min[i][j], min[i][k] + min[k + 1][j]);
-                            max[i][j] = Math.max(max[i][j], max[i][k] + max[k + 1][j]);
-                            break;
-                        case '-':
-                            min[i][j] = Math.min(min[i][j], min[i][k] - max[k + 1][j]);
-                            max[i][j] = Math.max(max[i][j], max[i][k] - min[k + 1][j]);
-                    }
-                }
-            }
-        }
-        
-        System.out.println(min[0][n - 1]);
-
+        return res;
     }
 }
