9598a35348c433f88c8ac2bc36e97172479f3dc1
viroovr <tjgusdnjs789@gmail.com>
2025-12-24 11:01:20 +0900

    [Refactor] 2816

--- boj/2816/Main.java
+++ boj/2816/Main.java
@@ -3,46 +3,37 @@ import java.util.*;
 
 public class Main {
 
+    static final char UP = '1';
+    static final char SELECT = '4';
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         
         int N = Integer.parseInt(br.readLine());
 
-        int kbs1 = 0, kbs2 = 0;
+        int kbs1 = -1, kbs2 = -1;
+
         for (int i = 0; i < N; i++) {
             String channel = br.readLine();
-            if (channel.equals("KBS1")) {
+            if (channel.equals("KBS1")) 
                 kbs1 = i;
-            }
-            else if (channel.equals("KBS2")) {
+            if (channel.equals("KBS2")) 
                 kbs2 = i;
-
-            }
-        }
-        // System.out.println(kbs1);
-        // System.out.println(kbs2);
-        
-        for (int i = 0; i < kbs1; i++) {
-            sb.append("1");
-        }
-        for (int i = 0; i < kbs1; i++) {
-            sb.append("4");
         }
 
-        if ( kbs2 < kbs1) {
+        move(sb, kbs1, kbs1);
+
+        if ( kbs2 < kbs1)
             kbs2++;
-        }
-        if (kbs2 != 1) {
-            for (int i = 0; i < kbs2; i++) {
-                sb.append("1");
-            }
-            for (int i = 1; i < kbs2; i++) {
-                sb.append("4");
-            }
-        }
+
+        move(sb, kbs2, kbs2 - 1);
         
-        // System.out.println(sb.length());
         System.out.println(sb.toString());
+    }
+
+    private static void move(StringBuilder sb, int up, int select) {
+        for (int i = 0; i < up; i++) sb.append(UP);
+        for (int i = 0; i < select; i++) sb.append(SELECT);
     }
 }
